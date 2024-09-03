7cf4757e1ab88c468bb0507f89f46bbdb617c60a
Christopher White <bc_white@outlook.com>
2024-09-02 23:12:44 -0400

    Adding requirements file and updating skill_compression to emit skills to a file.

--- scripts/python/skill_compression.py
+++ scripts/python/skill_compression.py
@@ -39,18 +39,39 @@ def process_args() -> argparse.Namespace:
     return parser.parse_args()
 
 def ingest_skills(skills_file: str) -> List[str]:
-    """Ingest skills from a file
+    """Ingest skills from a file, splitting sentences on commas and semicolons
     Args:
         skills_file (str): Path to the skills file
     Returns:
         List[str]: List of skills
+    
+    Raises:
+        FileNotFoundError: If the skills file is not found
     """
     logging.info('Ingesting skills from %s...', skills_file)
     skills = []
-    with open(skills_file, 'r', encoding='utf-8') as src_skills:
-        for line in src_skills:
-            skills.append(line.strip())
-    return skills
+    try:
+        with open(skills_file, 'r', encoding='utf-8') as src_skills:
+            for line in src_skills:
+                for skill in line.strip().replace(';', ',').split(','):
+                    skills.append(skill.strip())
+        return skills
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(f'Skills file: {skills_file} not found') from exc
+
+def output_skills(skills: List[str], dest_skills_file: str) -> None:
+    """Output skills to a file
+    Args:
+        skills (List[str]): List of skills
+        dest_skills_file (str): Path to the destination skills file
+    """
+    logging.info('Outputting skills to %s...', dest_skills_file)
+    try:
+        with open(dest_skills_file, 'w', encoding='utf-8') as dest_skills:
+            for skill in skills:
+                dest_skills.write(f'{skill}\n')
+    except PermissionError as exc:
+        raise PermissionError(f'Permission denied writing: {dest_skills_file}') from exc
 
 def main(args: argparse.Namespace) -> None:
     """Main function
@@ -60,8 +81,16 @@ def main(args: argparse.Namespace) -> None:
     logging.info('Downloading NLTK stopwords...')
     nltk_download('stopwords', quiet=True)
     stop_words = set(stopwords.words('english'))
-    skills = ingest_skills(args.src_skill_file)
-    # print(skills)
+    try:
+        skills = ingest_skills(args.src_skill_file)
+    except FileNotFoundError as exc:
+        logging.error(exc)
+        sys.exit(1)
+    try:
+        output_skills(skills, args.dest_skill_file)
+    except PermissionError as exc:
+        logging.error(exc)
+        sys.exit(1)
     # print(stop_words)
 
 if __name__ == '__main__':
