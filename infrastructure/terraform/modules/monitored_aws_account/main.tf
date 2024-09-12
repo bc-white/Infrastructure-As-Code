9392e92d476b90cf98eeb3d93cfb2118ee4cdef9
Christopher White <bc_white@outlook.com>
2024-09-12 00:00:24 -0400

    Updating control policies to allow for publishing of budgetary information to an SNS topic.

--- infrastructure/terraform/modules/monitored_aws_account/main.tf
+++ infrastructure/terraform/modules/monitored_aws_account/main.tf
@@ -12,6 +12,26 @@ resource "aws_sns_topic" "admin_notifications" {
   display_name = "Admin Notifications"
   kms_master_key_id = "alias/aws/sns"
   name              = "admin-notifications"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid: "Allow Budget Connections To SNS",
+        Effect = "Allow"
+        Principal = "*"
+        Action = "sns:Publish"
+        Resource = "arn:aws:sns:${data.aws_caller_identity.current.id}:${data.aws_caller_identity.current.id}:*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceOwner" = data.aws_caller_identity.current.account_id
+          },
+          "ArnLike": {
+            "aws:SourceArn": "arn:aws:budgets::${data.aws_caller_identity.current.account_id}:*"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_sns_topic_subscription" "email_admin_notifications_subscription" {
