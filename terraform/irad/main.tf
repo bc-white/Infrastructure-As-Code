79511ef7bab32e88cac3d3181e7bd66f52660cdf
Christopher White <bc_white@outlook.com>
2025-03-08 19:58:50 -0500

    Updating VPC subnets.

--- terraform/irad/main.tf
+++ terraform/irad/main.tf
@@ -62,10 +62,10 @@ module "tf_state_bucket" {
 module "vpc" {
     source               = "terraform-aws-modules/vpc/aws"
     name                 = "irad-eks-vpc"
-    cidr                 = "10.0.0.0/16"
+    cidr                 = "10.1.0.0/16"
     azs                  = slice(data.aws_availability_zones.available_zones.names, 0, 3)
-    private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    private_subnets      = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+    public_subnets       = ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24"]
     enable_nat_gateway   = true
     single_nat_gateway   = true
     enable_dns_hostnames = true
