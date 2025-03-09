b531e80bbeeb336d10e1b885c7c052d5e48c15cf
Christopher White <bc_white@outlook.com>
2025-03-08 19:59:32 -0500

    Updating node groups for EKS cluster

--- terraform/irad/main.tf
+++ terraform/irad/main.tf
@@ -106,7 +106,7 @@ module "eks" {
             name = "node-group-2"
             instance_types = ["t3.small"]
             min_size     = 1
-            max_size     = 2
+            max_size     = 3
             desired_size = 1
         }
     }
