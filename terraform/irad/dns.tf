307177ff2879b32adabdf8ec3fd5d28984ac8f74
Christopher White <bc_white@outlook.com>
2025-05-04 15:10:30 -0400

    Adding settings for DNS and updating provider settings that have changed.

---
+++ terraform/irad/dns.tf
@@ -0,0 +1,30 @@
+module "r53_zones" {
+    source    = "terraform-aws-modules/route53/aws//modules/zones"
+    zones     = {
+        "terraform-aws-modules-example.com" = {
+            comment = "bogartlab.com"
+            tags    = {
+                env = "production"
+            }
+        }
+    }
+}
+
+# module "r53_records" {
+#     source    = "terraform-aws-modules/route53/aws//modules/records"
+#     zone_name = keys(module.r53_zones.route53_zone_zone_id)[0]
+#     depends_on = [module.zones]
+#     records = []
+# }
+
+resource "aws_kms_key" "route_53_ksk_kms_key" {
+    description = "KMS key for Route 53 KSK"
+    deletion_window_in_days = 7
+    enable_key_rotation = true
+    tags = {
+        Name = "Route 53 KSK KMS Key"
+        Environment = "prod"
+        Terraform = "true"
+        Account = "IRAD"
+    }
+}
