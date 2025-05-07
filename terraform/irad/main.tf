9c9ef7fda3998f996f50727750278a2e47fdc8f5
Christopher White <bc_white@outlook.com>
2025-05-06 22:49:22 -0400

    Downsizing.

--- terraform/irad/main.tf
+++ terraform/irad/main.tf
@@ -109,14 +109,14 @@ module "eks" {
     eks_managed_node_groups = {
         one = {
             name = "node-group-1"
-            instance_types = ["m7i.2xlarge"]
+            instance_types = ["m7i.xlarge"]
             min_size     = 1
             max_size     = 3
             desired_size = 2
         }
         two = {
             name = "node-group-2"
-            instance_types = ["m7i.2xlarge"]
+            instance_types = ["m7i.xlarge"]
             min_size     = 1
             max_size     = 3
             desired_size = 1
