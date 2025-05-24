bbb6afeba04f851c28e0d9222b0df72b5a8f2043
Christopher White <bc_white@outlook.com>
2025-05-24 16:22:43 -0400

    Updating install command for helm.

--- charts/gitlab/helm-install.ps1
+++ charts/gitlab/helm-install.ps1
@@ -2,5 +2,4 @@ helm install gitlab gitlab/gitlab `
     -f gitlab-values.yml `
     --version 9.0.1 `
     --timeout 600s `
-    --namespace gitlab `
-    --set installCertmanager=false
+    --namespace gitlab
