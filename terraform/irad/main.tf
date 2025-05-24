0181ac339b181958ef0cf1bbe9d019b2726c709b
Christopher White <bc_white@outlook.com>
2025-05-24 17:34:11 -0400

    Adding default storage that was missed initially and updating README.

--- terraform/irad/main.tf
+++ terraform/irad/main.tf
@@ -166,6 +166,26 @@ module "irsa-ebs-csi" {
     oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
+# Default StorageClass for EBS volumes
+resource "kubernetes_storage_class" "ebs_gp3" {
+    metadata {
+        name = "ebs-gp3"
+        annotations = {
+            "storageclass.kubernetes.io/is-default-class" = "true"
+        }
+    }
+    storage_provisioner    = "ebs.csi.aws.com"
+    reclaim_policy        = "Delete"
+    volume_binding_mode   = "WaitForFirstConsumer"
+    allow_volume_expansion = true
+    parameters = {
+        type      = "gp3"
+        encrypted = "true"
+        fsType    = "ext4"
+    }
+    depends_on = [module.eks]
+}
+
 # AWS Load Balancer Controller IAM Role
 module "aws_load_balancer_controller_irsa_role" {
     source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
