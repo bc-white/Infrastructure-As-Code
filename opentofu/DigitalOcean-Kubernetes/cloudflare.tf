28984875a937ac9b5be6942557d524ef8507b18f
Christopher White <bc_white@outlook.com>
2025-11-23 00:29:08 -0500

    Minor updates to cloudflare setup.

--- opentofu/DigitalOcean-Kubernetes/cloudflare.tf
+++ opentofu/DigitalOcean-Kubernetes/cloudflare.tf
@@ -1,16 +1,8 @@
-locals {
-  lb_ip       = try(data.kubernetes_service.ingress_nginx_controller.status[0].load_balancer[0].ingress[0].ip, null)
-  lb_hostname = try(data.kubernetes_service.ingress_nginx_controller.status[0].load_balancer[0].ingress[0].hostname, null)
-  lb_address  = coalesce(local.lb_ip, local.lb_hostname)
-  is_ipv4     = can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", local.lb_ip))
-}
-
 resource "cloudflare_dns_record" "ingress_controller_lb_dns_record" {
-  count   = local.lb_address != null ? 1 : 0
   zone_id = var.cloudflare_zone_id
   name    = var.cloudflare_record_name
-  type    = local.is_ipv4 ? "A" : "CNAME"
-  content = local.lb_address
-  ttl     = 300
+  type    = "A"
+  content = data.kubernetes_service.ingress_nginx_controller.status[0].load_balancer[0].ingress[0].ip
+  ttl     = 1
   proxied = true
 }
