a58749cb397db0718cd5cc21a611ea6b636d8c3b
Christopher White <bc_white@outlook.com>
2025-11-22 20:15:36 -0500

    Updated Tofu backend to utilize the newly created S3 bucket.

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -9,9 +9,19 @@ terraform {
       version = "~> 5"
     }
   }
+  backend "s3" {
+    bucket                      = "bcwhite-tech-opentofu-state"
+    key                         = "environment/development/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
 }
 
-resource "digitalocean_spaces_bucket" "foobar" {
+resource "digitalocean_spaces_bucket" "state-bucket" {
   name   = "bcwhite-tech-opentofu-state"
   region = "nyc3"
 }
