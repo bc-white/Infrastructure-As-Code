f228610f851035c9ed08261316cf704f0bc41208
Christopher White <bc_white@outlook.com>
2025-11-26 17:56:16 -0500

    Stubbing folder structure for Kubernetes certs.

---
+++ certifications/CKA/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+  backend "s3" {
+    bucket                      = "bcwhite-tech-opentofu-state"
+    key                         = "cka/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
+}
