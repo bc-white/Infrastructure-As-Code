597fdd4dd8eb3f6467a23543997292ab987f9f4b
Christopher White <bc_white@outlook.com>
2025-12-03 18:37:04 -0500

    Checkpointing pre-initialization write up.

--- certifications/CKA/scripts/worker.sh
+++ certifications/CKA/scripts/worker.sh
@@ -1,10 +1,28 @@
 #!/bin/env bash
-# This script sets up a worker node for a Kubernetes cluster.
+# This script sets up the control plane node for a Kubernetes cluster.
+
+# Stage Kubernetes repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | \
+  gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | \
+  tee /etc/apt/sources.list.d/kubernetes.list
+
 
 # Update and install necessary packages
 apt-get update
 apt-get upgrade -y
-apt-get install -y apt-transport-https ca-certificates curl
+apt-get install -y apt-transport-https \
+  ca-certificates \
+  containerd \
+  curl \
+  gpg \
+  kubeadm \
+  kubectl \
+  kubelet
+apt-mark hold kubeadm kubectl kubelet
+
+# Startup Kubelet service
+systemctl enable --now kubelet
 
 # Ensure swap is disabled
 swapoff -a
