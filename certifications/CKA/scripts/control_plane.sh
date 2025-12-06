a7ff51275c050e4948775e682cfb129093021d47
Christopher White <bc_white@outlook.com>
2025-12-05 19:18:12 -0500

    spacing tweak.

--- certifications/CKA/scripts/control_plane.sh
+++ certifications/CKA/scripts/control_plane.sh
@@ -12,7 +12,6 @@
 ###############################################################################
 # Configure Base Linux System
 ###############################################################################
-
 # Stage Docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
