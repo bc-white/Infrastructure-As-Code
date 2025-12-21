2d8d350984071ff03dc7e909c5808191b237b3ed
Christopher White <bc_white@outlook.com>
2025-12-21 15:22:51 -0500

    Starting namespace constraint lab

---
+++ certifications/CKA/scripts/namespace-limits.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+###############################################################################
+# This script creates a namespace that is intended to have low resource
+# limitations put into place to demonstrate constraints. It is cobbled together
+# from things I have learned from various sources including:
+# - Kubernetes documentation
+# - CKA curriculum
+# - Various GitHub repositories, blogs, and tutorials
+# - Personal experience
+###############################################################################
+
+###############################################################################
+# Create the namespace
+###############################################################################
