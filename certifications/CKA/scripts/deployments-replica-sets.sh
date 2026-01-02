fc677f7a82618b1ebcf7d390269f13cc56739ee8
Christopher White <bc_white@outlook.com>
2026-01-02 13:35:11 -0500

    Updating examples.

--- certifications/CKA/scripts/deployments-replica-sets.sh
+++ certifications/CKA/scripts/deployments-replica-sets.sh
@@ -35,3 +35,41 @@ spec:
 EOF
 
 kubectl apply -f replica-set.yaml
+
+###############################################################################
+# Create a Deployment
+###############################################################################
+
+kubectl create deploy webserver --image nginx:1.22.1 --replicas=2 \
+--dry-run=client -o yaml | tee deployment.yaml
+kubectl apply -f deployment.yaml
+kubectl set image deploy webserver nginx=nginx:1.23.1-alpine --record
+kubectl rollout history deploy webserver
+kubectl rollout history deploy webserver --revision=1
+kubectl rollout undo deploy webserver
+
+
+###############################################################################
+# DaemonSet Example
+###############################################################################
+cat <<EOF > daemon-set.yaml
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: ds-one
+spec:
+  selector:
+    matchLabels:
+      app: ds-one
+  template:
+    metadata:
+      labels:
+        app: ds-one
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.22.1
+        ports:
+        - containerPort: 80
+EOF
+kubectl apply -f daemon-set.yaml
