329ee27d1158f8041ab766e5a92516e9c8e8c767
Christopher White <bc_white@outlook.com>
2026-01-03 16:44:29 -0500

    Adding PVs and PVCs

--- certifications/CKA/scripts/secrets-configmaps-persistent_volumes.sh
+++ certifications/CKA/scripts/secrets-configmaps-persistent_volumes.sh
@@ -46,3 +46,49 @@ kubectl apply -f configmap-pod.yaml
 kubectl exec -it configmap-demo -- printenv FAVORITE_COLOR
 kubectl delete pod configmap-demo
 kubectl delete configmap colors
+
+###############################################################################
+# Creating Persistent Volumes
+###############################################################################
+# Setting up NFS for PVs
+apt-get update
+apt-get install -y nfs-kernel-server nfs-common
+mkdir -p /srv/nfs/kubedata
+chown nobody:nogroup /srv/nfs/kubedata
+chmod 1777 /srv/nfs/kubedata
+echo "/srv/nfs/kubedata *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+exportfs -ra
+systemctl restart nfs-kernel-server
+mount -t nfs localhost:/srv/nfs/kubedata /mnt
+# Create Persistent Volume
+cat <<EOF > pv-nfs.yaml
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-vol-1
+spec:
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  nfs:
+    path: /srv/nfs/kubedata
+    server: k8s-control-plane
+    readOnly: false
+EOF
+kubectl apply -f pv-nfs.yaml
+# Create Persistent Volume Claim
+cat <<EOF > pvc-nfs.yaml
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: pvc-vol-1
+spec:
+  accessModes:
+    - ReadWriteMany
+  resources:
+    requests:
+      storage: 200Mi
+EOF
+kubectl apply -f pvc-nfs.yaml
