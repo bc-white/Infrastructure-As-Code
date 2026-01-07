cf31933d10abcc1319f0d9a8e8dada5412cfb834
Christopher White <bc_white@outlook.com>
2026-01-06 21:12:40 -0500

    Checking in working version

--- certifications/CKA/scripts/routing.sh
+++ certifications/CKA/scripts/routing.sh
@@ -102,3 +102,84 @@ kubectl apply -f https://raw.githubusercontent.com/nginx/\
 kubectl get all -n nginx-gateway
 # Patch the NGINX Gateway service to be NodePort type
 kubectl patch service/nginx-gateway -n nginx-gateway -p '{"spec": {"type": "NodePort"}}'
+
+# Setup Deployments and Services to be exposed via Gateway API
+cat <<EOF > gateway-api-deployments.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: movies
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: movies
+  strategy: {}
+  template:
+    metadata:
+      labels:
+        app: movies
+    spec:
+      containers:
+      - image: nginx
+        name: movies
+        ports:
+        - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: movies
+spec:
+  ports:
+  - port: 80
+    targetPort: 80
+    protocol: TCP
+    name: http
+  selector:
+    app: movies
+EOF
+kubectl apply -f gateway-api-deployments.yaml
+
+# Setup Gateway API resources
+cat <<EOF > gateway-resources.yaml
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: blockbuster
+spec:
+  gatewayClassName: nginx
+  listeners:
+  - name: http
+    protocol: HTTP
+    port: 80
+EOF
+kubectl apply -f gateway-resources.yaml
+kubectl get gateway
+
+# Setup HTTPRoute to route traffic to services
+cat <<EOF > httproute.yaml
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: blockbuster-route
+spec:
+  parentRefs:
+  - name: blockbuster
+  hostnames:
+  - "blockbuster.example.com"
+  rules:
+  - matches:
+    - path:
+        type: PathPrefix
+        value: /
+    backendRefs:
+    - name: movies
+      port: 80
+EOF
+kubectl apply -f httproute.yaml
+kubectl get httproute
+
+# Test it out
+export HOST=$(hostname -i | awk '{print $2}')
+curl --resolve blockbuster.example.com:32500:$HOST http://blockbuster.example.com:32500/
