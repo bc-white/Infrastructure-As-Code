232cd5304a5e2690a39f5ea3878bb74b5dc95cef
Christopher White <bc_white@outlook.com>
2026-01-09 12:58:40 -0500

    Stubbing lab for taints, tolerations, and scheduling.

---
+++ certifications/CKA/scripts/labels-taints-tolerations.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+###############################################################################
+# This script exercises labels, taints, tolerations, and scheduling.
+# It is cobbled together from things I have learned from various sources
+# including:
+# - Kubernetes documentation
+# - CKA curriculum
+# - Various GitHub repositories, blogs, and tutorials
+# - Personal experience
+###############################################################################
